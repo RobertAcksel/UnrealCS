232f8580a602f972576719b00087b5479a19fe33
Robert Acksel <robert.acksel@googlemail.com>
2017-08-04 20:02:32 +0200

    -translate

--- Engine/Plugins/UnrealCS/UECSharpDomain/copy/Program.cs
+++ Engine/Plugins/UnrealCS/UECSharpDomain/copy/Program.cs
@@ -5,90 +5,62 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace copy
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            if(args.Length<2)
-            {
-                Console.Out.WriteLine("此程序会自动将源文件复制到目标目录下，如果目标");
-                 Console.Out.WriteLine("目录已经有相同的文件，则更改源文件名，增加更高的数字后缀");
-                Console.Out.WriteLine("请指定复制源文件和目标目录：");
-                Console.Out.WriteLine("参数1：源文件");
-                Console.Out.WriteLine("参数2:目标目录");
+namespace copy{
+    class Program{
+        static void Main(string[] args){
+            if (args.Length < 2){
+                Console.Out.WriteLine("This program will automatically copy the source file to the target directory. If the target directory already has the same file, change the source file name and add a higher numeric suffix. Specify the copy source file and destination directory: Parameter 1: Source file parameter 2 : Target directory");
                 return;
             }
-            Console.Out.WriteLine(string.Format("复制 {0} 到 {1}",args[0],args[1]));
+            Console.Out.WriteLine($"Copy {args[0]} to {args[1]}");
 
-            string fileName;
-            int indexPath = args[0].LastIndexOf('/');
-            int indexDll = args[0].LastIndexOf('.');
+            var indexPath = args[0].LastIndexOf('/');
+            var indexDll = args[0].LastIndexOf('.');
 
-            if (indexDll == -1)
-            {
+            if (indexDll == -1){
                 indexDll = args[0].Length;
                 args[0] += ".dll";
             }
 
-            if (indexPath != -1)
-            {
-                fileName = args[0].Substring(indexPath, indexDll - indexPath);
-            }
-            else
-                fileName = args[0].Substring(0, indexDll);
+            var fileName = indexPath != -1 ? args[0].Substring(indexPath, indexDll - indexPath) : args[0].Substring(0, indexDll);
 
+            var targetFolder = new DirectoryInfo(args[1]);
 
-            DirectoryInfo targetFolder = new DirectoryInfo(args[1]);
-
-
-            if(!Directory.Exists(args[1]))
-            {
+            if (!Directory.Exists(args[1])){
                 Directory.CreateDirectory(args[1]);
             }
 
-            Console.Out.WriteLine("检测最新文件...");
+            Console.Out.WriteLine("Detect the latest file ...");
 
-            int max_number = 0;
-            FileInfo[] files = targetFolder.GetFiles();
-            foreach (FileInfo fi in files)
-            {
-                int startIndex = fi.Name.LastIndexOf('-') + 1;
-                int endIndex = fi.Name.LastIndexOf('.');
-                string f = fi.Name.Substring(startIndex, endIndex - startIndex);
-                try
-                {
-                    int num = Int32.Parse(f);
-                    if (num >= max_number)
-                    {
+            var max_number = 0;
+            var files = targetFolder.GetFiles();
+            foreach (var fi in files){
+                var startIndex = fi.Name.LastIndexOf('-') + 1;
+                var endIndex = fi.Name.LastIndexOf('.');
+                var f = fi.Name.Substring(startIndex, endIndex - startIndex);
+                try{
+                    var num = Int32.Parse(f);
+                    if (num >= max_number){
                         max_number = num + 1;
                     }
+                } catch (Exception e){
+                    Console.Out.WriteLine(fi.Name + " The name is wrong：" + e.Message);
                 }
-                catch (Exception e)
-                {
-                    Console.Out.WriteLine(fi.Name + " 名称错误：" + e.Message);
-                }
-
             }
 
-            //目录补上反斜杠
+            //Directory make up the backslash
             if (args[1][args[1].Length - 1] != '/')
                 args[1] += "/";
 
-            string targetFileName = args[1] + fileName +"-"+ max_number + ".dll";
+            var targetFileName = args[1] + fileName + "-" + max_number + ".dll";
 
             //string targetFileName = args[1] + args[0];
-            try
-            {
-                File.Copy(args[0], targetFileName,true);
-                Console.Out.WriteLine("复制成功 " + targetFileName);
+            try{
+                File.Copy(args[0], targetFileName, true);
+                Console.Out.WriteLine("Copy successful " + targetFileName);
+            } catch (Exception e){
+                Console.Out.WriteLine("Copy failed:" + e.Message);
             }
-            catch(Exception e)
-            {
-                Console.Out.WriteLine("复制失败:"+e.Message);
-            }
-            
         }
     }
 }
